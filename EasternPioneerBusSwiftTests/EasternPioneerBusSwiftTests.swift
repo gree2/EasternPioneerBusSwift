4f50e9e8805cdd2e22b8efa0cc83fcb310d93cf9
hqlgree2 <hqlgree2@gmail.com>
2015-03-14 15:40:38 +0800

    add xcode project

---
+++ EasternPioneerBusSwiftTests/EasternPioneerBusSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  EasternPioneerBusSwiftTests.swift
+//  EasternPioneerBusSwiftTests
+//
+//  Created by hqlgree2 on 14/3/15.
+//  Copyright (c) 2015 hqlgree2. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class EasternPioneerBusSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
